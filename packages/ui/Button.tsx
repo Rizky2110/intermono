c8d3e1bbc3755176948ccf99eebc7e6b8578e2df
interads@DESKTOP-VKGQQDG <interads.dekstop-vkgqqdg@interads.com>
2022-08-24 15:26:20 +0700

    initial material button

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,5 +1,21 @@
-import { PACKAGE_NAME } from "core";
-import * as React from "react";
+import { hexToHsl, PACKAGE_NAME } from "core";
+import styled from "styled-components";
+
+const StyledButton = styled("button")`
+  font-size: 1.5rem;
+  background-color: #e8204f;
+  color: ${(props) => props.theme.colors?.primary || "white"};
+
+  &:hover {
+    background-color: ${(props) => hexToHsl("#e8204f", 10)};
+  }
+`;
+
 export const Button = () => {
-  return <button>{PACKAGE_NAME}</button>;
+  const hsl = hexToHsl("#ffffff", 0.2);
+  return (
+    <StyledButton>
+      {PACKAGE_NAME}, {hsl}
+    </StyledButton>
+  );
 };
